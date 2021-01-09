90399b25939892faebdba96f85174feaee5354dc
m-cheicki <mcheicki.dev@gmail.com>
2021-01-09 01:03:43 +0100

    API with all 19 models + index.html

--- app.py
+++ app.py
@@ -1,15 +1,133 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import requests
 import numpy as np
 import pickle as p
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='views')
 
-@app.route('/api/', methods=["POST"])
-def post_predict():
+
+@app.route('/', methods=["GET"])
+def home():
+    return render_template('index.html')
+
+
+@app.route('/alcohol/', methods=["POST"])
+def alcohol():
+    model = p.load(open('models/ALCOHOL_CONSUMPTION_prediction.pickle', 'rb'))
     return jsonify(np.array2string(model.predict([request.get_json()['person']])))
 
+
+@app.route('/amphet/', methods=["POST"])
+def amphet():
+    model = p.load(open('models/AMPHET_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/amyl/', methods=["POST"])
+def amyl():
+    model = p.load(open('models/AMYL_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/benzos/', methods=["POST"])
+def benzos():
+    model = p.load(open('models/BENZOS_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/caffeine/', methods=["POST"])
+def caffeine():
+    model = p.load(open('models/CAFFEINE_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/cannabis/', methods=["POST"])
+def cannabis():
+    model = p.load(open('models/CANNABIS_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/chocolate/', methods=["POST"])
+def chocolate():
+    model = p.load(
+        open('models/CHOCOLATE_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/coke/', methods=["POST"])
+def coke():
+    model = p.load(open('models/COKE_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/crack/', methods=["POST"])
+def crack():
+    model = p.load(open('models/CRACK_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/ectasy/', methods=["POST"])
+def ectasy():
+    model = p.load(open('models/ECSTASY_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/heroin/', methods=["POST"])
+def heroin():
+    model = p.load(open('models/HEROIN_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/ketamine/', methods=["POST"])
+def ketamine():
+    model = p.load(open('models/KETAMINE_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/legal-highs/', methods=["POST"])
+def lh():
+    model = p.load(
+        open('models/LEGAL_HIGHS_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/lsd/', methods=["POST"])
+def lsd():
+    model = p.load(open('models/LSD_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/magic-mushrooms/', methods=["POST"])
+def mm():
+    model = p.load(
+        open('models/MAGIC_MUSHROOMS_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/meth/', methods=["POST"])
+def meth():
+    model = p.load(open('models/METH_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/nicotine/', methods=["POST"])
+def nicotine():
+    model = p.load(open('models/NICOTINE_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/semer/', methods=["POST"])
+def semer():
+    model = p.load(open('models/SEMER_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
+@app.route('/vsa/', methods=["POST"])
+def vsa():
+    model = p.load(open('models/VSA_CONSUMPTION_prediction.pickle', 'rb'))
+    return jsonify(np.array2string(model.predict([request.get_json()['person']])))
+
+
 if __name__ == '__main__':
-    model = p.load(open('models/svm_prediction.pickle', 'rb'))
     app.run(debug=True, host='localhost', port='5000')
--- request.py
+++ request.py
@@ -2,14 +2,36 @@ import requests
 import numpy as np
 import json
 
-url = 'http://localhost:5000/api/'
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 
 person = [-0.07854, 0.48246, -1.43719, 0.96082, -0.31685,
           3.27393, -2.11437, -0.84732, -1.07533, -2.30408, 0.52975, -1.18084]
 j_data = json.dumps({'person': person})
-# to_predict = np.array(list(json.values())).astype(float)
 
-r = requests.post(url, data=j_data, headers=headers)
-print(r.url)
-print(r.text)
+alcohol = 'http://localhost:5000/alcohol/'
+amphet = 'http://localhost:5000/amphet/'
+amyl = 'http://localhost:5000/amyl/'
+benzos = 'http://localhost:5000/benzos/'
+caffeine = 'http://localhost:5000/caffeine/'
+cannabis = 'http://localhost:5000/cannabis/'
+chocolate = 'http://localhost:5000/chocolate/'
+coke = 'http://localhost:5000/coke/'
+crack = 'http://localhost:5000/crack/'
+heroin = 'http://localhost:5000/heroin/'
+ketamine = 'http://localhost:5000/ketamine/'
+legal_highs = 'http://localhost:5000/legal-highs/'
+lsd = 'http://localhost:5000/lsd/'
+magic_mushrooms = 'http://localhost:5000/magic-mushrooms/'
+meth = 'http://localhost:5000/meth/'
+nicotine = 'http://localhost:5000/nicotine/'
+semer = 'http://localhost:5000/semer/'
+vsa = 'http://localhost:5000/vsa/'
+
+drugs = [alcohol, amphet, amyl, benzos,
+         caffeine, cannabis, chocolate, coke, crack, heroin, ketamine,
+         legal_highs, lsd, magic_mushrooms, meth, nicotine, semer, vsa]
+
+
+for drug in drugs:
+    r = requests.post(drug, data=j_data, headers=headers)
+    print(f"{drug[22:-1]} : " + r.text)
